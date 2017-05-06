8764e6c14b599cef79e5d91a9df11bf2961ba640
Akhila G P <akhilapadmanaban@gmail.com>
2017-05-06 15:40:33 +0530

    Create class1.py

---
+++ class1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+class Employee:#common base class
+    empCount = 0
+    
+    def __init__(self, name, salary): # Initialization or constructor
+        self.name = name
+        self.salary = salary
+        Employee.empCount += 1 #class variable
+        
+    def displayCount(self):#class method
+        print "Total Employee %d", Employee.empCount
+        
+    def displayEmployee(self):
+        print "Name: ",self.name, "Salary: ", self.salary
+        
+emp1 = Employee("Krishna","2000")#instance object
+emp2 = Employee("Dharma","4000")
+emp1.displayEmployee()#accessing attributes
+emp2.displayEmployee()

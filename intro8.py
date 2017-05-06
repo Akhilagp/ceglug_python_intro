3247b59f7dfca6741ad5d0b5519e101537ee7453
Akhila G P <akhilapadmanaban@gmail.com>
2017-05-06 15:18:34 +0530

    Update intro8.py

--- intro8.py
+++ intro8.py
@@ -3,6 +3,6 @@
 file1 = open('test.txt')
 for line in file1:
     print line    
-file2 = open('test2.txt','w')
+file2 = open('tezt2.txt','w')
 line2 = "This is a text from the program \n"
 file2.write(line2)
